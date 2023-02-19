1762872dfb01d4d60530ea8d8804ec4b7fae9f5c
SR10 <sunnahrafiq2177@gmail.com>
2023-02-19 00:32:01 +0000

    Basic UIs for Landing and register pages created

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 import sqlite3
 app = Flask(__name__)
 @app.route("/")
-@app.route('/login')
 def login():
   return render_template('login.html')
+@app.route("/register")
+def register():
+  return render_template('register.html')
+@app.route('/landing')
+def landing():
+  return render_template('landing.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
