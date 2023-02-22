de7d7ef593908a707513408cea1517d5d0d74bf4
SR10 <sunnahrafiq2177@gmail.com>
2023-02-22 01:42:41 +0000

    login page functionality added (database)

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import sqlite3
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/', methods=['POST', 'GET'])
 def login():
-  return render_template('login.html')
+    if request.method=='POST':
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        name=request.form['uname']
+        password=request.form['psw']
+        query="SELECT name,password FROM users where name ='"+name+"' and password='"+password+"'"
+        cur.execute(query)
+        results=cur.fetchall()
+        if len(results)==0:
+            print('SORRY INCORRECT CREDENTIALS TRY AGAIN')
+        else:
+            return render_template('landing.html')
+    return render_template('login.html')
+
 @app.route("/register")
 def register():
   return render_template('register.html')
